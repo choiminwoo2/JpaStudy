ad8d05f46d0c6d57c65bcb4b71cf6e96acb8eeea
wjrdjs005 <wjrdjs005@naver.com>
2023-02-08 23:53:37 +0900

    v4dto직접조회

--- springJpaVersion2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ springJpaVersion2/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Path;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +24,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
 
+    private final OrderQueryRepository orderQueryRepository;
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orederV1(){
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -68,6 +72,10 @@ public class OrderSimpleApiController {
         return result;
     }
 
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDtos();
+    }
     @Data
     static class OrderDto{
     private Long orderId;
